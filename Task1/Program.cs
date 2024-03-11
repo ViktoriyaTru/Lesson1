217c989bcc9a10a09c1a8a10b62273129acb3cae
ViktoriyaTru <viktoriya.trubitsina@gmail.com>
2024-03-11 21:56:42 +0300

    add some variables lesson: task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,11 @@
-﻿System.Console.WriteLine("Hello World");
+﻿//System.Console.WriteLine("Hello World");
+
+int value = 100;
+System.Console.WriteLine(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+System.Console.WriteLine(d);
+System.Console.WriteLine(c);
+System.Console.WriteLine(b);
